78edfc80cc183c53c2059c04666dd92d3db88c3e
Idongesit <weliidongesit@gmail.com>
2024-04-22 11:09:31 +0100

    Effects changes for milestone_3.py

---
+++ milestone_2.py
@@ -0,0 +1,16 @@
+import random
+
+def word_list():
+    return word_list
+
+word_list = ["Apple", "Grapes", "Kiwi", "Pear", "Banana"]
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+guess = input("Enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha() == True:
+    print("Good guess")
+else:
+    print("Oops! That is not a valid input.")
